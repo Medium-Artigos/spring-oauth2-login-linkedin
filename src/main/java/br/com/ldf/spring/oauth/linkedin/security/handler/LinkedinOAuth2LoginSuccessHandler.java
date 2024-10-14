1c0f49778cc57ef2d65b17c42e4aa6abc117bcd0
lucas_fernandes <lucas.rj.fernandes@gmail.com>
2024-10-14 16:20:26 -0300

    ajuste

--- src/main/java/br/com/ldf/spring/oauth/linkedin/security/handler/LinkedinOAuth2LoginSuccessHandler.java
+++ src/main/java/br/com/ldf/spring/oauth/linkedin/security/handler/LinkedinOAuth2LoginSuccessHandler.java
@@ -1,9 +1,5 @@
 package br.com.ldf.spring.oauth.linkedin.security.handler;
 
-import br.com.ldf.spring.oauth.linkedin.persistence.entity.Role;
-import br.com.ldf.spring.oauth.linkedin.persistence.entity.User;
-import br.com.ldf.spring.oauth.linkedin.persistence.entity.UserRole;
-import br.com.ldf.spring.oauth.linkedin.persistence.repository.RoleRepository;
 import br.com.ldf.spring.oauth.linkedin.persistence.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,12 +9,10 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 
 import static br.com.ldf.spring.oauth.linkedin.controller.AuthenticationController.AUTHENTICATION_ENDPOINT_PATH;
@@ -30,7 +24,6 @@ import static br.com.ldf.spring.oauth.linkedin.controller.AuthenticationControll
 public class LinkedinOAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     UserRepository userRepository;
-    RoleRepository roleRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -38,34 +31,13 @@ public class LinkedinOAuth2LoginSuccessHandler implements AuthenticationSuccessH
         String email = ((OAuth2AuthenticationToken) authentication).getPrincipal().getAttribute("email");
         Objects.requireNonNull(email);
 
-        userRepository.findByEmail(email).ifPresentOrElse(
+        userRepository.findByEmail(email).ifPresent(
             user -> {
                 log.info("stage=on-authentication-success, message=person-found, person={}", user);
-                request.getSession().setAttribute("person", user);
-            }
-            , () -> {
-                var user = createNewUser((OAuth2User) authentication.getPrincipal());
-                log.info("stage=on-authentication-success, message=person-created, person={}", user);
                 request.getSession().setAttribute("person", user);
             });
 
         response.sendRedirect(AUTHENTICATION_ENDPOINT_PATH);
     }
 
-    private User createNewUser(OAuth2User oAuth2User) {
-        String name = Objects.requireNonNull(oAuth2User.getAttribute("name"));
-        String email = Objects.requireNonNull(oAuth2User.getAttribute("email"));
-
-        // por default caso o usuario não exista, ele será criado com o papel de usuário
-        var role = roleRepository.findByName(Role.ROLE_USER).orElseThrow(() -> new RuntimeException("Role not found"));
-
-        var user = User.builder()
-            .name(name)
-            .email(email)
-            .build();
-
-        user.setRoles(List.of(UserRole.builder().user(user).role(role).build()));
-
-        return userRepository.save(user);
-    }
 }
